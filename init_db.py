10a0d340c2af347d69831c1d6bea4cd4b9459dea
Tulho Melo <tulhomelo@users.noreply.github.com>
2023-03-14 00:08:15 +0000

    Documentando init_db

--- init_db.py
+++ init_db.py
@@ -1,13 +1,15 @@
 import sqlite3
 
+# Cria o banco de dados
 connection = sqlite3.connect('database.db')
 
-
+# Executa o schema
 with open('schema.sql') as f:
     connection.executescript(f.read())
 
 cur = connection.cursor()
 
+# Insere os registros
 cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
             ('First Post', 'Content for the first post')
         )
@@ -16,5 +18,7 @@ cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
             ('Second Post', 'Content for the second post')
         )
 
+# Grava as alterações na tabela
 connection.commit()
+# Encerra a conexão
 connection.close()
